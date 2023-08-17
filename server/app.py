be856c2a6b2ff26d731b55a31aa43644384feba3
sandyjtech <sandyjtech@gmail.com>
2023-08-17 09:39:53 -0700

    restful"

--- server/app.py
+++ server/app.py
@@ -16,8 +16,56 @@ api = Api(app)
 
 
 class Projects(Resource):
-    pass
+    ## INDEX
+    def get(self):
+        projects = [project.to_dict() for project in Project.query.all()]
+        return make_response(projects, 200)
 
+    ## CREATE
+    def post(self):
+        data = request.get_json()
+        new_project = Project(
+            name=data["name"],
+            details=data["details"],
+        )
+        ##new_project = Project(**data)
+        db.session.add(new_project)
+        db.session.commit()
+        return make_response(new_project.to_dict(), 201)
+
+
+api.add_resource(Projects, "/projects")
+
+
+class ProjectById(Resource):
+    ## SHOW
+    def get_by_id(self, id):
+        project = Project.query.filter(Project.id == id).first().to_dict()
+        return make_response(project, 200)
+
+    ## UPDATE
+    def patch(self, id):
+        data = request.get_json()
+        #project = Project.query.filter(Project.id == id).first()
+        project = Project.query.get_or_404(id)
+        
+        for key, value in data.items():
+            setattr(project, key, value)
+        db.session.add(project)
+        db.session.commit()
+
+        return make_response(project.to_dict(), 202)
+
+    ## DESTROY
+    def delete(self, id):
+        project = Project.query.filter(Project.id == id).first()
+        db.session.delete(project)
+        db.session.commit()
+
+        return make_response("", 204)
+
+
+api.add_resource(ProjectById, "/projects/<int:id>")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,8 +4,12 @@ from sqlalchemy_serializer import SerializerMixin
 db = SQLAlchemy()
 
 
-class Product(db.Model, SerializerMixin):
-    pass
+class Project(db.Model, SerializerMixin):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    detail = db.Column(db.String)
 
     def __repr__(self):
         pass
